289cd7494c5ce6e0a67228ebdff52820c391e416
Pietro Carozzi <pietro.karo@gmail.com>
2023-04-01 01:47:07 +0200

    Clean

--- GTAV bubble cars/scriptmain.cpp
+++ GTAV bubble cars/scriptmain.cpp
@@ -10,14 +10,28 @@
 
 using namespace std;
 
+bool test1;
+
 struct TouchedVehicle
 {
 	Vehicle vehicle;
-	Vector3 bubble_velocity;
 	bool first_touch;
+
+	explicit TouchedVehicle(Vehicle veh) : vehicle{ veh }, first_touch{ true } {}
 
 	bool operator ==(TouchedVehicle other) const { return vehicle == other.vehicle; }
 };
+
+inline void removeInvalidVeh(list<TouchedVehicle>& veh_list)
+{ 
+	veh_list.remove_if([](TouchedVehicle tv) { return !tv.vehicle.exist(); });
+}
+
+void resetTouchedVehs(list<TouchedVehicle>& veh_list)
+{
+	for (TouchedVehicle& tv : veh_list)
+		tv.first_touch = true;
+}
 
 void uniquePushBack(list<TouchedVehicle>& veh_list, TouchedVehicle item)	// Avoid duplicate elements
 {
@@ -34,86 +48,99 @@ void touchCheck(list<TouchedVehicle>& veh_list)
 		return;
 	for (Vehicle v : world_veh)
 		if (v != player_veh && v.isTouching(player_veh))
-			uniquePushBack(veh_list, TouchedVehicle{ v, player_veh.getVelocity(false), true });
+			uniquePushBack(veh_list, TouchedVehicle{ v });
 }
 
-void bubbleCars(list<TouchedVehicle>& veh_list, float velocityy)
+void bubbleCars(list<TouchedVehicle>& veh_list, float up_velocity)
 {
 	if (veh_list.size() >= 50)
 	{
-		veh_list.remove_if([](TouchedVehicle tv) { return !tv.vehicle.exist(); });	// Clear list from invalid vehicles deleted by RAGE
+		removeInvalidVeh(veh_list);
 		hud::showNotification("Superata soglia 50 veicoli, lista pulita.");
 	}
 	for (TouchedVehicle& tv : veh_list)
 	{
 		if (!tv.vehicle)
 			continue;	// Skip non persistent vehicles deleted by RAGE
-		float velocity;
-		Vector3 curr_velocity = tv.bubble_velocity;
+		Vector3 velocity = tv.vehicle.getVelocity(false);
+		float uv = up_velocity;
 		if (tv.first_touch)	// first_touch checking force vehicle to go up even if the player applies a force in down direction (example: player hit vehicle while is on a downhill road)
-		{
-
 			tv.first_touch = false;
-		}
-		
-		if (curr_velocity.z > 0.0f)
-		{
-
-		}
-
-
-		
-
-		else if (curr_velocity.z < 0.0f)
-			velocity *= -1;
-		curr_velocity.z = velocity;
-		tv.vehicle.setVelocity(curr_velocity);
+		else if (velocity.z <= 0.0f)
+			uv *= -1;
+		velocity.z = uv;
+		tv.vehicle.setVelocity(velocity);
 	}
 }
 
 void mainLoop()
 {
 	bool is_cheat_running = false;
+	int key_start = VK_NUMPAD1;
+	int key_stop = VK_NUMPAD0;
+	int key_bubble_all = VK_NUMPAD3;
 	list<TouchedVehicle> touched_vehs;
 	float up_velocity = 1.0f;
 	bool clear_list = true;
 	while (true)
 	{
-		if (IsKeyDown(VK_NUMPAD1) && !is_cheat_running)
+		if (IsKeyDown(key_start) && !is_cheat_running)
 		{
 			scriptWait(200);
 			is_cheat_running = true;
-			hud::printMessage("BUBBLECARS cheat activated!");
+			if (!clear_list)
+			{
+				removeInvalidVeh(touched_vehs);
+				
+				hud::showNotification("Lista pulita.");
+			}
+			hud::printMessage("BUBBLECARS cheat activated!", 2000, true);
 		}
+
+
 		if (IsKeyDown(VK_NUMPAD4))
 		{
 			scriptWait(200);
 			Vehicle bar = createVehicle("Barracks", Ped::player().getPosition() + Ped::player().getForwardVector() * 5);
-			bar.modifyTopSpeed(100.0f);
+			bar.modifyTopSpeed(130.0f);
 		}
+		if (IsKeyDown(VK_NUMPAD6))
+		{
+			scriptWait(200);
+			hud::showNotification((int)touched_vehs.size());
+		}
+
+
 		if (is_cheat_running)
 		{
-			if (IsKeyDown(VK_NUMPAD0))
-			{
-				scriptWait(200);
-				is_cheat_running = false;
-				if (clear_list)
-					touched_vehs.clear();
-				hud::printMessage("BUBBLECARS cheat deactivated!");
-				continue;
-			}
-			else if (IsKeyDown(VK_NUMPAD3))
+			if (IsKeyDown(key_bubble_all))
 			{
 				scriptWait(200);
 				touched_vehs.clear();
 				for (Vehicle v : getAllVehicles<256>())
 					if (v != Ped::player().getCurrentVehicle())
-						//touched_vehs.push_back(TouchedVehicle{ v,  });
+						touched_vehs.push_back(TouchedVehicle{ v });
 				bubbleCars(touched_vehs, up_velocity);
 				continue;
 			}
 			touchCheck(touched_vehs);
 			bubbleCars(touched_vehs, up_velocity);
+			if (IsKeyDown(key_stop))
+			{
+				scriptWait(200);
+				if (clear_list)
+				{
+					touched_vehs.clear();
+					hud::showNotification("Lista pulita.");
+				}
+				else
+				{
+					resetTouchedVehs(touched_vehs);
+					hud::showNotification("Lista resettata.");
+				}
+				hud::printMessage("BUBBLECARS cheat deactivated!", 2000, true);
+				is_cheat_running = false;
+			}
 		}
 		scriptWait(0);
 	}
