2a12c8140b6d25d6fc7aa5cd6362dbc85e95fa3f
Pietro Carozzi <pietro.karo@gmail.com>
2023-04-08 01:49:02 +0200

    Added functions to read script parameters

--- GTAV bubble cars/scriptmain.cpp
+++ GTAV bubble cars/scriptmain.cpp
@@ -1,6 +1,8 @@
 #include <Windows.h>
 #include <list>
 #include <algorithm>
+#include <fstream>
+#include <sstream>
 #include "inc/main.h"
 #include "inc/keyboard.h"
 #include "inc/native_wrapper.h"
@@ -13,15 +15,6 @@ using namespace std;
 constexpr short BUBBLECARS_MAX = 256;
 constexpr short TOUCHED_MAX = 50;
 
-struct InitData
-{
-	int key_start = VK_NUMPAD1;
-	int key_stop = VK_NUMPAD0;
-	int key_bubble_all = VK_NUMPAD3;
-	float up_velocity = 1.0f;
-	bool clear_list = true;
-};
-
 struct TouchedVehicle
 {
 	Vehicle vehicle;
@@ -32,25 +25,71 @@ struct TouchedVehicle
 	bool operator ==(TouchedVehicle other) const { return vehicle == other.vehicle; }
 };
 
-inline void removeInvalidVehs(list<TouchedVehicle>& veh_list)
-{ 
-	veh_list.remove_if([](TouchedVehicle tv) { return !tv.vehicle.exist(); });
+// Simple "read value" function
+float readVelParam(const string& line)
+{
+	istringstream iss{ line };
+	iss.ignore(11);
+	float velocity;
+	if (!(iss >> velocity) || velocity < 0.5f || velocity > 50.0f)
+	{
+		hud::showNotification("Up velocity config value is invalid. Default will be used.");
+		velocity = 1.0f;
+	}
+	return velocity;
 }
 
-void resetTouchedVehs(list<TouchedVehicle>& veh_list)
+// Simple "read value" function
+bool readClearParam(const string& line)
 {
-	for (TouchedVehicle& tv : veh_list)
+	istringstream iss{ line };
+	iss.ignore(6);
+	string valstr;
+	if (iss >> valstr)
+	{
+		transform(valstr.begin(), valstr.end(), valstr.begin(), toupper);
+		if (valstr == "TRUE")
+			return true;
+		else if (valstr == "FALSE")
+			return false;
+	}
+	hud::showNotification("Clear config value is invalid. Default will be used.");
+	return true;
+}
+
+pair<float, bool> readScriptData()
+{
+	ifstream ifs{ "GTAVBubbleCars.ini" };
+	if (!ifs)
+	{
+		hud::showNotification("Unable to read script config file. Default config will be used");
+		return pair<float, bool>(1.0f, false);
+	}
+	string line1, line2;
+	if (!getline(ifs, line1) || !getline(ifs, line2))
+		hud::showNotification("Error while reading config values. Default values will be used");
+	return pair<float, bool>{ readVelParam(line1), readClearParam(line2) };
+}
+
+inline void removeInvalidVehs(list<TouchedVehicle>& vehList)
+{ 
+	vehList.remove_if([](TouchedVehicle tv) { return !tv.vehicle.exist(); });
+}
+
+void resetTouchedVehs(list<TouchedVehicle>& vehList)
+{
+	for (TouchedVehicle& tv : vehList)
 		tv.first_touch = true;
 }
 
-void uniquePushBack(list<TouchedVehicle>& veh_list, TouchedVehicle item)	// Avoid duplicate elements
+void uniquePushBack(list<TouchedVehicle>& vehList, TouchedVehicle item)	// Avoid duplicate elements
 {
-	if (find(veh_list.begin(), veh_list.end(), item) != veh_list.end())
+	if (find(vehList.begin(), vehList.end(), item) != vehList.end())
 		return;
-	veh_list.push_back(item);
+	vehList.push_back(item);
 }
 
-void touchCheck(list<TouchedVehicle>& veh_list)
+void touchCheck(list<TouchedVehicle>& vehList)
 {
 	auto world_veh = getAllVehicles<BUBBLECARS_MAX>();
 	Vehicle player_veh = Ped::player().getCurrentVehicle();
@@ -58,10 +97,10 @@ void touchCheck(list<TouchedVehicle>& veh_list)
 		return;
 	for (Vehicle v : world_veh)
 		if (v != player_veh && v.isTouching(player_veh))
-			uniquePushBack(veh_list, TouchedVehicle{ v });
+			uniquePushBack(vehList, TouchedVehicle{ v });
 }
 
-void bubbleCars(list<TouchedVehicle>& veh_list, float up_velocity)
+void bubbleCars(list<TouchedVehicle>& veh_list, float upVelocity)
 {
 	if (veh_list.size() >= TOUCHED_MAX)
 	{
@@ -73,12 +112,12 @@ void bubbleCars(list<TouchedVehicle>& veh_list, float up_velocity)
 		if (!tv.vehicle)
 			continue;			// Skip non persistent vehicles deleted by RAGE
 		Vector3 velocity = tv.vehicle.getVelocity(false);
-		float uv = up_velocity;
+		float up_velocity = upVelocity;
 		if (tv.first_touch)		// first_touch checking force vehicle to go up even if the player applies a force in down direction (example: player hit vehicle while is on a downhill road)
 			tv.first_touch = false;
 		else if (velocity.z <= 0.0f)
-			uv *= -1;
-		velocity.z = uv;
+			up_velocity *= -1;
+		velocity.z = up_velocity;
 		tv.vehicle.setVelocity(velocity);
 	}
 }
@@ -86,30 +125,28 @@ void bubbleCars(list<TouchedVehicle>& veh_list, float up_velocity)
 void mainLoop()
 {
 	bool is_cheat_running = false;
-	InitData data;
+	int key_start = VK_NUMPAD1;
+	int key_stop = VK_NUMPAD0;
+	int key_bubble_all = VK_NUMPAD3;
+	auto script_data = readScriptData();
+	hud::showNotification(to_string(script_data.first) + "   " + to_string(script_data.second));
 	list<TouchedVehicle> touched_vehs;
 	while (true)
 	{
-		if (IsKeyDown(data.key_start) && !is_cheat_running)
+		if (IsKeyDown(key_start) && !is_cheat_running)
 		{
 			scriptWait(200);
 			is_cheat_running = true;
-			if (!data.clear_list)
+			if (!script_data.second)
 			{
 				removeInvalidVehs(touched_vehs);
 				
-				hud::showNotification("Lista pulita.");
+				hud::showNotification("Lista pulita da veicoli invalidi.");
 			}
 			hud::printMessage("BUBBLECARS cheat activated!", 2000, true);
 		}
 
 
-		if (IsKeyDown(VK_NUMPAD4))
-		{
-			scriptWait(200);
-			Vehicle bar = createVehicle("Barracks", Ped::player().getPosition() + Ped::player().getForwardVector() * 5);
-			bar.modifyTopSpeed(130.0f);
-		}
 		if (IsKeyDown(VK_NUMPAD6))
 		{
 			scriptWait(200);
@@ -119,25 +156,25 @@ void mainLoop()
 
 		if (is_cheat_running)
 		{
-			if (IsKeyDown(data.key_bubble_all))
+			if (IsKeyDown(key_bubble_all))
 			{
 				scriptWait(200);
 				touched_vehs.clear();
 				for (Vehicle v : getAllVehicles<BUBBLECARS_MAX>())
 					if (v != Ped::player().getCurrentVehicle())
 						touched_vehs.push_back(TouchedVehicle{ v });
-				bubbleCars(touched_vehs, data.up_velocity);
+				bubbleCars(touched_vehs, script_data.first);
 				continue;
 			}
 			touchCheck(touched_vehs);
-			bubbleCars(touched_vehs, data.up_velocity);
-			if (IsKeyDown(data.key_stop))
+			bubbleCars(touched_vehs, script_data.first);
+			if (IsKeyDown(key_stop))
 			{
 				scriptWait(200);
-				if (data.clear_list)
+				if (script_data.second)
 				{
 					touched_vehs.clear();
-					hud::showNotification("Lista pulita.");
+					hud::showNotification("Lista pulita da tutti i veicoli.");
 				}
 				else
 				{
